9a3e66badbaf01799cc6305ae11f00703a5c3164
Lipat JJ <joblipat@gmail.com>
2023-04-17 11:47:44 +0000

    implemented solution

--- playground.js
+++ playground.js
@@ -1,15 +1,51 @@
-/* 
-// Metacrafters - Beginning Javascript
-//
-// This is a javascript playground for testing your javascript code!
-// When you are ready to test, simply right click and select "Run Code"
-// to see the result print below. If you have more then one snippet of code,
-// you can highlight the code you want to test, and then right click and select "Run Code"
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
 */
 
-// Enter your code below this line
+// create a variable to hold your NFT's
+const nfts = [];
 
-// Code example
-for(var i=1; i<=5; i++) {
- console.log(i);
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT(id, name, color, ownerName) {
+    nft = {
+        id: id,
+        name: name,
+        color: color,
+        ownerName: ownerName
+    }
+
+    nfts.push(nft)
+    console.log("Minted: " + name)
 }
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs() {
+    nfts.forEach((nft) => {
+        console.log("Id: " + nft.id)
+        console.log("Name: " + nft.name)
+        console.log("Color: " + nft.color)
+        console.log("Owner Name: " + nft.ownerName)
+        console.log()
+    })
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log(nfts.length)
+}
+
+// call your functions below this line
+mintNFT(1, "Sapphire", "Blue", "Joe Biden")
+mintNFT(2, "Ruby", "Red", "Donald Trump")
+mintNFT(3, "Emerald", "Green", "Barack Obama")
+listNFTs()
+getTotalSupply()
